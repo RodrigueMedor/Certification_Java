11b387fb5e81a7f4806f010124ef4c6d2cf0fc1b
rmedor <rmedor@nexient.com>
2021-06-20 21:44:52 -0400

    certification

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+A
+start-Foo/pom.xml,8\f\8ffa8e16b14899d0570374dceab01c9eca1ccf7c
+Y
+)start-Foo/src/main/java/start/FooBar.java,1\f\1f7b89d06b44d4b3d2880dc556f8adf3b16cb4c6
+V
+&start-Foo/src/main/java/start/Bar.java,a\6\a64d9090f23b73602359050dd58b26554355bb7b
+W
+'start-Foo/src/main/java/start/Foo2.java,3\f\3fa06e8d41fdcab68cb952b07d4fa19b98142fbc
+V
+&start-Foo/src/main/java/start/Foo.java,b\b\bb3fe52ec9d21d2719fb9cc60a0e4c7ca9f9cd50
---
+++ start-Foo/src/main/java/start/Foo.java
@@ -0,0 +1,98 @@
+package start;
+
+public class Foo {
+
+    public static void main(String[] args) {
+        // Declaration of local variables, local to the main method
+        boolean myBoolean;
+        byte myByte;
+        char myFirstChar, mySecondChar;
+        short myShort;
+        long myLong;
+        float myFloat;
+        double myFirstDouble, mySecondDouble;
+
+        // You can set all of the numeric primitive data variables to a literal number
+        // from 0 to 127.
+        byte b = 127;
+        char c = 127;
+        short s = 127;
+        int i = 127;
+        long l = 127;
+        float f = 127;
+        double d = 127;
+
+        // boolean can only be set to true or false
+        boolean isTrue = false;
+
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+        System.out.println("i = " + i);
+        System.out.println("l = " + l);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("isTrue = " + isTrue);
+
+        long l2 = 127l;
+        long l3 = 127L;
+        System.out.println("l2 = " + l2);
+        System.out.println("l2 = " + l3);
+
+        float f2 = 127f;
+        float f3 = 127F;
+        System.out.println("f2 = " + f2);
+        System.out.println("f3 = " + f3);
+
+        double d2 = 127d;
+        double d3 = 127D;
+        System.out.println("d2 = " + d2);
+        System.out.println("d3 = " + d3);
+
+        float f4 = 1.27e02f;
+        double d4 = 1.27e02;
+        System.out.println("f4 = " + f4);
+        System.out.println("d4 = " + d4);
+
+        byte b4 = 0b1111111;   // binary 127 prefix 0b
+        System.out.println("b4 = " + b4);
+
+        short s4 = 0177;  // octal 127 - octal prefix 0 only
+        System.out.println("s4 = " + s4);
+
+        int i4 = 0x007F; // hexidecimal 127 - hex prefix 0x
+        System.out.println("i4 = " + i4);
+
+//        byte b5 = 'a';
+//        char c5 = 'a';
+//        double d5 = 'a';
+//
+//        System.out.println("b5 = " + b5);
+//        System.out.println("c5 = " + c5);
+//        System.out.println("d5 = " + d5);
+
+        byte b5 = 'a';
+        char c5 = 'a';
+        double d5 = 'a';
+
+        System.out.println("b5 = " + b5);
+        System.out.println("c5 = " + c5);
+        System.out.println("d5 = " + d5);
+
+        byte b7 = 0b0000_0001;
+        char c7 = 0x0_07F;
+        short s7 = 011_11;
+        int i7 = 1_000_000;
+        long d7 = 1_000_000L;
+        float f7 = 1_000.000_000f;
+        double l7 = 1.000_0000e10;
+
+        System.out.println("b7 = " + b7);
+        System.out.println("c7 = " + c7);
+        System.out.println("s7 = " + s7);
+        System.out.println("i7 = " + i7);
+        System.out.println("l7 = " + l7);
+        System.out.println("f7 = " + f7);
+        System.out.println("d7 = " + d7);
+    }
+}
